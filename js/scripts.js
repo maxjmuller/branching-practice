554bf56fbd8b63bc0bf6e4d28bb71a95f2617502
Max Muller & Anithasrini <student@epicodus.com>
2015-10-12 19:34:51 -0700

    Add text to index.html and make Javascript prompt for age.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $(".btn").click(function() {
+    var height = parseInt(prompt("What is your height in feet?"));
+
+    if (height >= 4) {
+      $("#tall").show();
+      $("#short").show();
+    } else {
+      $("#short").show();
+    }
+  });
+});
